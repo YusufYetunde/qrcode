67a96aae002e8bdee3552bc5b7da2432ee22c51b
Yusuf Yetunde <yusufyetunde13@gmail.com>
2022-03-05 19:41:34 -0800

    second commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function Footer() {
+  return (
+    <footer>
+      <div className="attribution sticky mt-10 bottom-0 text-center h-10 w-full flex items-center justify-center">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://www.frontendmentor.io/profile/YusufYetunde" target="_blank">Yusuf Yetunde</a>.
+      </div>
+    </footer>
+  );
+}
